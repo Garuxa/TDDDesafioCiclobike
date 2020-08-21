8dae00e24feef9bcd224516f72ee348a4e748b04
Gary Hermosilla <garuxa@gmail.com>
2020-08-20 22:15:17 -0400

    Hito RecyclerView 4: - Crear el adapter (usando el ViewHolder)

--- app/src/main/java/cl/desafiolatam/tdddesafiociclobike/Adapter.kt
+++ app/src/main/java/cl/desafiolatam/tdddesafiociclobike/Adapter.kt
@@ -1,6 +1,21 @@
 package cl.desafiolatam.tdddesafiociclobike
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class CyclobikeViewholder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+class CycloAdapter : RecyclerView.Adapter<CyclobikeViewholder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CyclobikeViewholder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: CyclobikeViewholder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
