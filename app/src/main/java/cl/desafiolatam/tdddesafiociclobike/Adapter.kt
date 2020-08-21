2afd81115370caaf713b7d030eca004673e65d0c
Gary Hermosilla <garuxa@gmail.com>
2020-08-20 22:06:45 -0400

    Hito RecyclerView 3: - Crear archivo kotlin (en este caso llamado Adapter.kt) - En ese archivo crear la clase que extiende el ViewHolder

---
+++ app/src/main/java/cl/desafiolatam/tdddesafiociclobike/Adapter.kt
@@ -0,0 +1,6 @@
+package cl.desafiolatam.tdddesafiociclobike
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class CyclobikeViewholder(itemView: View) : RecyclerView.ViewHolder(itemView)
