b535d2956d4b61c62107f002794064ff8c03f309
Gary Hermosilla <garuxa@gmail.com>
2020-08-21 00:02:10 -0400

    Hito RecyclerView 4.3: - Agregar ids para identificar los TextView del layout list_item.xml - Agregar las referencias a los TextView definidos en el layout de list_item.xml al ViewHolder - Luego, en onBindViewHolder, asignamos los valores a los textView del viewHolder

--- app/src/main/java/cl/desafiolatam/tdddesafiociclobike/Adapter.kt
+++ app/src/main/java/cl/desafiolatam/tdddesafiociclobike/Adapter.kt
@@ -1,11 +1,21 @@
 package cl.desafiolatam.tdddesafiociclobike
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class CyclobikeViewholder(itemView: View) : RecyclerView.ViewHolder(itemView)
+class CyclobikeViewholder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val tvCiclovia: TextView
+    val tvComuna: TextView
+
+    init {
+        tvCiclovia = itemView.findViewById(R.id.tvCiclovia)
+        tvComuna = itemView.findViewById(R.id.tvComuna)
+    }
+}
 
 class CycloAdapter(val lista: List<Ciclovia>) : RecyclerView.Adapter<CyclobikeViewholder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CyclobikeViewholder {
@@ -14,7 +24,12 @@ class CycloAdapter(val lista: List<Ciclovia>) : RecyclerView.Adapter<CyclobikeVi
     }
 
     override fun onBindViewHolder(holder: CyclobikeViewholder, position: Int) {
-        TODO("Not yet implemented")
+        val ciclovia = lista[position]
+        Log.d("CycloAdaoter", ciclovia.toString())
+
+        holder.tvCiclovia.text = ciclovia.nombre
+        holder.tvComuna.text = ciclovia.comuna
+
     }
 
     override fun getItemCount(): Int {
